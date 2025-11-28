05d8be4f3c7ee1b5a19ab9b9802f2592600b7d1f
Victor Macalin <victormacalin07@gmail.com>
2025-11-28 16:47:58 -0300

    chore: inclui versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,13 +2,10 @@ package dev.java10x.cadastroDeNinjas.Ninjas;
 
 import dev.java10x.cadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.security.PrivilegedAction;
 import java.util.List;
-@Data
 @Entity
 @Table(name = "TB_CADASTRO")
 @NoArgsConstructor
@@ -17,21 +14,58 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private Long id;
 
-    @Column(name = "nome")
-    private String name;
+    private String nome;
 
     @Column(unique = true)
     private String email;
 
-    @Column(name = "idade")
-    private int age;
+    private int idade;
 
 
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
 }
